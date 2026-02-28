f07ad84bf3e628cbc80368ff5dd6698e12d3e3ab
ZhiXingtong <u3656761@connect.hku.hk>
2026-02-28 12:24:56 +0800

    SPADER Example

---
+++ app/SPADE_sender.py
@@ -0,0 +1,47 @@
+'''
+sending message from an agent
+'''
+import spade
+from spade.agent import Agent
+from spade.behaviour import OneShotBehaviour
+from spade.message import Message
+from spade.template import Template
+
+
+class SenderAgent(Agent):
+    class InformBehav(OneShotBehaviour):
+        async def run(self):
+            print("InformBehav running")
+            msg = Message(to="receiver@your_xmpp_server")     # Instantiate the message
+            msg.set_metadata("performative", "inform")  # Set the "inform" FIPA performative
+            msg.set_metadata("ontology", "myOntology")  # Set the ontology of the message content
+            msg.set_metadata("language", "OWL-S")       # Set the language of the message content
+            msg.body = "Hello World"                    # Set the message content
+
+            await self.send(msg)
+            print("Message sent!")
+
+            # set exit_code for the behaviour
+            self.exit_code = "Job Finished!"
+
+            # stop agent from behaviour
+            await self.agent.stop()
+
+    async def setup(self):
+        print("SenderAgent started")
+        self.b = self.InformBehav()
+        self.add_behaviour(self.b)
+
+
+async def main():
+    senderagent = SenderAgent("sender@your_xmpp_server", "sender_password")
+    await senderagent.start(auto_register=True)
+    print("Sender started")
+
+    await spade.wait_until_finished(receiveragent)
+    print("Agents finished")
+
+
+if __name__ == "__main__":
+    spade.run(main())
+
